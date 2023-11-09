12af409f20f8fc59a0f27995ec90fc6ee72ae5a8
K0SS4 <github@k0ss4.xyz>
2023-11-09 16:41:33 +0100

    Change repo name to iso-downloader. Removed unnecessary(for me) features. Code cleanup

--- README.md
+++ README.md
@@ -1,27 +1,13 @@
-## linux-downloader
-Bash script for autodownloading of different latest, "bleeding edge" Linux distros and testing them in qemu VM. Other than Linux, it also boots *BSDs families and other opensource projects. Also, booting from iPXE mirrors (netboot.xyz) is supported.
-
-## Linux Distrohopper Dream Machine Script
-```
-/----------------------------------------------------------------------------------\
-| Script downloads recent (latest release) linux ISOs and spins a VM for a test.   |
-| This is distrohopper dream machine. It consist of the file with distro download  | 
-| functions (distrofunctions.sh) as well as this script. Theoretically, the script | 
-| should always download recent linux ISOs without any updates. But, if the dev(s) |
-| change the download URL or something else, it might be required to do manual     |
-| changes - distrofunctions.sh.                                                    |
-\----------------------------------------------------------------------------------/
-```
+## iso-downloader
+Bash script for autodownloading of different linux distros, bsd distros or other OSes useful for diagnostics etc.
 
 ## Requirements: 
 * Basic stuff: `linux`, `bash`, `curl`, `wget`, `awk`, `grep`, `xargs`, `pr`, `tail`, `sort` (these tools usually are preinstalled on linux)
 * Additional depencency: `html2text`
-* If you want to run a VM after downloading, you'll need `QEMU`
-* Install all deps on Arch-based distro: `sudo pacman -S html2text qemu`
-* Some distros are shared as archive so you'll need `unzip`
+* Install all deps on Arch-based distro: `sudo pacman -S html2text`
+* Some systems are shared as archive so you'll need `unzip`
 
-## Currently supported distributions
-If you can't find your favourite linux distro in here, please create a github issue with details about it or add support for it yourself and do a pull request.
+## Currently supported systems
 ```
 Arch-based:		    DEB-based:			RPM-based:		    BSD:			Other:		Not linux:
 0 = archlinux		    2 = debian			12 = fedora		    14 = freebsd		17 = memtest64bit	20 = freedos
@@ -37,53 +23,19 @@ Arch-based:		    DEB-based:			RPM-based:		    BSD:			Other:		Not linux:
 ```
 
 ## How to use?
-1. `git clone https://github.com/k0ss4/linux-downloader`
-2. `cd linux-downloader`
+1. `git clone https://github.com/k0ss4/iso-downloader`
+2. `cd iso-downloader`
 3. `./download.sh`
-If you manually pick distros (opt. one or two) you will be prompted about launching a VM for test spin for each distro.
-Multiple values are also supported:
-* one distribution (e.g. type 0 for archlinux)*
-* several distros - space separated (e.g. for getting both Arch and Endeavour, type '0 1' (without quotes))*
-* 'all' option, the script will ONLY download ALL of the ISOs (warning: this can take a lot of space as well as several hours to download everything!)
+Then you have couple of options:
+* download one ISO (e.g. type 0 for archlinux)*
+* download several ISOs - space separated (e.g. for getting both Arch and Endeavour, type '0 1' (without quotes))*
+* 'all' option, the script will download ALL of the ISOs (warning: this can take a lot of space as well as several hours to download everything!)
 * 'filesize' option will check the local (downloaded) filesizes of ISOs vs. the current/recent ISOs filesizes on the websites
 * 'netbootxyz' option allows you to boot from netboot.xyz via network
 
-## How to add distribution yourself
-0. Clone the repo, if you hadn't already, and enter the resuling folder: 
-```
-git clone https://github.com/sxiii/linux-downloader && cd linux-downloader
-```
-1. Open file "download.sh" with your favourite text editor. In the according row of "Categories", add the name of your distro in the end of the array.
-2. In the same file, little bit down further, add your distro variable (array) like this: (the order is the same as in "Categories")
-```
-distroname=("Full Distro Name" "arch" "releasename" "distronameurl")
-```
-Here is the real example to make it more obvious:
-```
-obarun=("Obarun" "amd64" "rolling" "obarunurl")
-```
-3. Save & close the "download.sh" file. Now, open "distrofunctions.sh" file. This is more tricky. We need to AUTOMATICALLY get the URL of the most recent distribution (fixed-links are forbidden as they will destroy the idea behind this script of always get the recent release). So you need to find a way to get the recent release full download URL. For example, if we want to download from Sourceforge.net, make a new function like this:
-```
-distronameurl () {
-mirror="https://sourceforge.net/projects/SF-Project-Name/files/latest/download"
-new="$mirror"
-output="distroname.iso"
-checkfile $1
-}
-```
-Here you will need to edit: function name (distronameurl), mirror address (in this example we're downloading project "SF-Project-Name" from sourceforge), output file name. Checkfile is the function that checks if file exists and downloads the file if not, so don't touch it. If you are going to download not from sourceforge, but from some other website, please check the other distributions for examples - there are plenty of examples of how to parse websites to get the recent download links automatically. In the end, you need to supply "new" variable with full ISO URL, as well as "output" var with filename (and mirror variable is there for cleaniness of the code and code consistancy). Other than that, you can use "grep, awk, xargs" or other classic UNIX tools to parse the HTML (or several files) and get the actual download link. If you'll create temporary files, please don't forget to remove them in the end of the function. You can as well parse file listings like HTTP or FTP server mirror hosts. If you can't write the correct code, feel free to create an issue and ask me for help.
-
-4. Now, save the "distrofunctions.sh" file. Run the ./download.sh script, type-in number of your distribution, and check, that it's downloaded correctly.
-5. Last but very important, please create a pull request, so I could check and add distribution of your choice.
-
-Thank you!
-
-## Notes on files
+## Files descriptions
 * download.sh is in the main script
-* distrofunctions.sh contains all URL/mirror/HTTP scraping stuff
-
-Feel free to do a pull request or ask me to add your favourite distro in the issues.
-NB: I prefer the distros updated at least during the last 2-3 years (ideally, 1 year). Exceptions can be historically important releases and rare/special operating systems.
+* systemfunctions.sh contains all URL/mirror/HTTP scraping stuff
 
 ## Author & License
 * Written by SecurityXIII
--- download.sh
+++ download.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
 
-helpsection () {
-echo "/----------------------------------------------------------------------------------------------------------------------------------------\ "
-echo "| Script downloads recent (latest release) linux ISOs and spins a VM for a test. This is kinda distrohopper dream machine.               | "
-echo "| It consist of the file with distro download functions (distrofunctions.sh) as well as this script (download.sh).                       | "
-echo "| Theoretically, the script should always download recent linux ISOs without any updates. But, if the developer(s)                       | "
-echo "| change the download URL or something else, it might be required to do manual changes - probably in distrofunctions.sh.                 | "
-echo "| Requirements: linux, bash, curl, wget, awk, grep, xargs, pr (these tools usually are preinstalled on linux)                            | "
-echo "| Some distros are shared as archive. So you'll need xz for guix, bzip2 for minix, zip for haiku & reactos, and, finally 7z for kolibri. | "
-echo "| Written by SecurityXIII / Aug 2020 ~ Jan 2023 / Kopimi un-license /--------------------------------------------------------------------/ "
-echo "\-------------------------------------------------------------------/"
-echo "+ How to use?"
-echo " If you manually pick distros (opt. one or two) you will be prompted about launching a VM for test spin for each distro."
-echo " Multiple values are also supported. Please choose one out of five options:"
-echo "* one distribution (e.g. type 0 for archlinux)*"
-echo "* several distros - space separated (e.g. for getting both Arch and Debian, type '0 4' (without quotes))*"
-echo "* 'all' option, the script will ONLY download ALL of the ISOs (warning: this can take a lot of space (100+GB) !)"
-echo "* 'filesize' option will check the local (downloaded) filesizes of ISOs vs. the current/recent ISOs filesizes on the websites"
-echo "* 'netbootxyz' option allows you to boot from netboot.xyz via network"
+helpsection ()
+{
+    echo "+ How to use?"
+    echo " You have a couple of options:"
+    echo "* download one ISO (e.g. type 0 for archlinux)*"
+    echo "* download several ISOs - space separated (e.g. for getting both Arch and Endeavour, type '0 1' (without quotes))*"
+    echo "* 'all' option, the script will download ALL of the ISOs (warning: this can take a lot of space!)"
+    echo "* 'filesize' option will check the local (downloaded) filesizes of ISOs vs. the current/recent ISOs filesizes on the websites"
+    echo "* 'netbootxyz' option allows you to boot from netboot.xyz via network"
 }
 
-# NB: I wanted to add ElementaryOS but the developers made it way too hard to implement auto-downloading.
-# If you can find constant mirror or place for actual release of ElementaryOS, please do a pull-request or just leave a comment.
-
-# "WIP". Todo:	1. Multiple architecture support;
-#		2. Multiple download mirror support;
-
-ram=1024 # Amount (mb) of RAM, for VM.
-cmd="qemu-system-x86_64" # The name of the qemu file to launch
-
-# Load the functions from distrofunctions.sh:
-. distrofunctions.sh
+# Load the functions from systemfunctions.sh:
+. systemfunctions.sh
 
 # Categories
 arch=(archlinux endeavour)
@@ -42,10 +24,10 @@ notlinux=(freedos netbootxyz)
 
 # All distributions
 category_names=("Arch-based" "DEB-based" "RPM-based" "BSD" "Other" "Not linux")
-distro_all=("arch" "deb" "rpm" "bsd" "other" "notlinux")
-distro_arr=("${arch[@]}" "${deb[@]}" "${rpm[@]}" "${bsd[@]}" "${other[@]}" "${notlinux[@]}")
+types=("arch" "deb" "rpm" "bsd" "other" "notlinux")
+system_arr=("${arch[@]}" "${deb[@]}" "${rpm[@]}" "${bsd[@]}" "${other[@]}" "${notlinux[@]}")
 
-# Legend ## Distroname ## Arch  ## Type     ## Download URL function name
+# Legend ## Systemname ## Arch ## Type ## Download URL function name
 
 # Archlinux-based distros
 archlinux=("ArchLinux" "amd64" "rolling" "archurl")
@@ -81,158 +63,133 @@ antiviruslive=("Antivirus Live CD" "amd64" "release" "antivirusliveurl")
 freedos=("FreeDOS" "amd64" "release" "freedosurl")
 netbootxyz=("netboot.xyz" "amd64" "release" "netbootxyz")
 
-drawmenu () {
+drawmenu ()
+{
+    q=0;
 
- q=0;
+    for ((i=0; i<${#types[@]}; i++)); do
+        col+="${category_names[$i]}: \n"
+        dist=${types[$i]}
+        typeset -n arr=$dist
 
- for ((i=0; i<${#distro_all[@]}; i++)); do
-	 col+="${category_names[$i]}: \n"
-	 dist=${distro_all[$i]}
-	 typeset -n arr=$dist
-	 for ((d=0; d<${#arr[@]}; d++)); do
-		 col+="$q = ${arr[$d]} \n"
-		 (( q++ ));
-	 done
- printf "$col" > col$i.tmp
- col=""
- done
+        for ((d=0; d<${#arr[@]}; d++)); do
+            col+="$q = ${arr[$d]} \n"
+            (( q++ ));
+        done
 
- pr -m -t -w170 col*tmp && rm *tmp
+        printf "$col" > col$i.tmp
+        col=""
+    done
 
+    pr -m -t -w170 col*tmp && rm *tmp
 }
 
-normalmode () {
-	drawmenu
-	echo "Please choose distro to download (type-in number or space-separated multiple numbers):"
-	read x 
-			
-	# Happens if the input is empty
-	if [ -z "$x" ]; then echo "Empty distribution number. Please type-in number of according distro. Exiting"; exit; fi # "Empty" handling
-	
-	# This questions are asked ONLY if user hadn't used the option "all".
-	if [ "$x" != "all" ] && [ "$x" != "filesize" ] && [ "$x" != "netbootxyz" ] && [ "$noconfirm" != "1" ]; then
-		for distr in $x; do 
-		dist=${distro_arr[$distr]}
-		typeset -n arr=$dist
-	
-		echo "You choose ${arr[0]} distro ${arr[2]}, built for ${arr[1]} arch. Do you want to download ${arr[0]} ISO? (y / n)"
-		read z
-		if [ $z = "y" ]; then $"${arr[3]}"; fi
-		echo "${arr[0]} downloaded, do you want to spin up the QEMU? (y / n)"
-		read z
-	
-		if [ $z = "y" ]; then
-			isoname="$(echo ${arr[0]} | awk '{print tolower($0)}').iso"
-			if ! type $cmd > /dev/null 2>&1; then
-				echo "qemu seems not installed. Cannot run VM, skipping."
-			else
-			# Uncomment the following two rows (a1 & a2) and comment out others if you want to make QEMU HDD
-			# qemu-img create ./${arr[0]}.img 4G                                                # a1. Creating HDD (if you want changes to be preserved)
-			# qemu-system-x86_64 -hda ./${arr[0]}.img -boot d -cdrom ./${arr[0]}*.iso -m 1024   # a2. Booting from CDROM with HDD support (changes will be preserved)
-				[ -f ./$isoname ] && $cmd -boot d -cdrom ./$isoname -m $ram           # b1. This is liveCD boot without HDD (all changes will be lost)
-			# This is for floppy .IMG support
-			# [ ! -f ./$isoname ] && imgname="$(echo ${arr[0]} | awk '{print tolower($0)}').img" && [ -f ./$imgname ] && $cmd --fda ./$imgname -m $ram
-  				[ ! -f ./$isoname ] && imgname="$(echo ${arr[0]} | awk '{print tolower($0)}').img" && [ -f ./$imgname ] && $cmd -drive format=raw,file=$imgname -m $ram
-			fi
-		fi
-	
-		done
-	else
-	
-	# All handling: automatic download will happen if user picked "all" option, no questions asked.
-		if [ "$x" = "all" ]; then 
-			for ((i=0; i<${#distro_arr[@]}; i++)); do xx+="$i "; done; x=$xx; 
-			#for ((i=0; i<${#distro_arr[@]}; i++)); do 
-			for distr in $x; do 
-				dist=${distro_arr[$distr]}
-				typeset -n arr=$dist
-				$"${arr[3]}"
-			done
-		#done
-		fi
-		
-		if [ "$noconfirm" = "1" ]; then 
-			for distr in $x; do 
-				dist=${distro_arr[$distr]}
-				typeset -n arr=$dist
-				$"${arr[3]}"
-			done
-		#done
-		fi
-		
-	# Sizecheck handling: show the distribution file sizes
-		if [ "$x" = "filesize" ]; then 
-		for ((i=0; i<${#distro_arr[@]}; i++)); do xx+="$i "; done; x=$xx;
-		#for ((i=0; i<${#distro_arr[@]}; i++)); do 
-			for distr in $x; do 
-				dist=${distro_arr[$distr]}
-				typeset -n arr=$dist	
-				$"${arr[3]}" "filesize"
-			done
-		#done
-		fi
-		
-		if [ "$x" = "netbootxyz" ]; then
-			echo "Downloading netboot image from netboot.xyz, please wait..." && netbootxyz
-			echo "Loading netboot.xyz.iso..." && $cmd -boot d -cdrom netboot.xyz.iso -m $ram
-		fi
-	fi
+normalmode ()
+{
+    drawmenu
+    echo "Please choose ISO to download (type-in number or space-separated multiple numbers):"
+    read x 
+
+    # Happens if the input is empty
+    if [ -z "$x" ]; then echo "Empty system number. Please type-in number of according system. Exiting"; exit; fi
+
+    # This questions are asked ONLY if user hadn't used the option "all".
+    if [ "$x" != "all" ] && [ "$x" != "filesize" ] && [ "$x" != "netbootxyz" ] && [ "$noconfirm" != "1" ]; then
+        for sys in $x; do 
+            s=${system_arr[$sys]}
+            typeset -n arr=$s
+
+            echo "You chose ${arr[0]} system ${arr[2]}, built for ${arr[1]} arch. Do you want to download ${arr[0]} ISO? (y / n)"
+            read z
+            if [ $z = "y" ]; then $"${arr[3]}"; fi
+        done
+    else
+        # All handling: automatic download will happen if user picked "all" option, no questions asked.
+        if [ "$x" = "all" ]; then 
+            for ((i=0; i<${#system_arr[@]}; i++)); do xx+="$i "; done; x=$xx; 
+
+            for sys in $x; do 
+                s=${system_arr[$sys]}
+                typeset -n arr=$s
+                $"${arr[3]}"
+            done
+        fi
+
+        if [ "$noconfirm" = "1" ]; then 
+            for sys in $x; do 
+                s=${system_arr[$sys]}
+                typeset -n arr=$s
+                $"${arr[3]}"
+            done
+        fi
+
+        # Sizecheck handling: show the distribution file sizes
+        if [ "$x" = "filesize" ]; then 
+            for ((i=0; i<${#system_arr[@]}; i++)); do xx+="$i "; done;
+            x=$xx;
+
+            for sys in $x; do 
+                s=${system_arr[$sys]}
+                typeset -n arr=$s
+                $"${arr[3]}" "filesize"
+            done
+        fi
+
+        if [ "$x" = "netbootxyz" ]; then
+            echo "Downloading netboot image from netboot.xyz, please wait..." && netbootxyz
+            echo "Loading netboot.xyz.iso..." && $cmd -boot d -cdrom netboot.xyz.iso -m $ram
+        fi
+    fi
 }
 
-quickmode () {
-	IFS=,
-	for distr in $distros; do
-		dist=${distro_arr[$distr]}
-		typeset -n arr=$dist	
-		$"${arr[3]}"
-	done
-	exit 0;
+quickmode ()
+{
+    IFS=,
+    for sys in $picked; do
+        s=${system_arr[$sys]}
+        typeset -n arr=$s
+        $"${arr[3]}"
+    done
+    exit 0;
 }
 
-VALID_ARGS=$(getopt -o hysld: --long help,noconfirm,silent,list,distro: -- "$@")
+VALID_ARGS=$(getopt -o hyls: --long help,noconfirm,list,system: -- "$@")
 if [[ $? -ne 0 ]]; then
     exit 1;
 fi
 
 eval set -- "$VALID_ARGS"
-while [ : ]; do
-  case "$1" in
-    -h | --help)
-        helpsection
-        echo "Valid command line flags:"
-		echo "-h/--help: Show this help"
-        echo "-y/--noconfirm: Download specified distro without confirmation. "
-        echo "-s/--silent: Don't show help or extra info."
-        echo "-l/--list: List all the available distros with it's corresponding number."
-        echo "-d/--distro: Download distributions specified in the comma-separated list. Example: 0,2,34"
-        exit 0;
-        ;;
-    -y | --noconfirm)
-        echo "-y/--noconfirm option specified. Script will download specified distro without confirmation."
-        noconfirm=1
-        shift
-        ;;
-    -s | --silent)
-        echo "-s/--silent option specified. Script will not show help or extra info."
-        silent=1
-        shift
-        ;;
-    -l | --list)
-        echo "List of supported distros:"
-        drawmenu
-        exit 0;
-        ;;
-    -d | --distro)
-        echo "-d/--distro option specified. Script will download distributions with the following numbers: '$2'"
-        distros="$2"
-        quickmode
-        ;;
-    --) shift; 
-        break 
-        ;;
-  esac
+while true
+do
+    case "$1" in
+        -h | --help)
+            helpsection
+            echo "Valid command line flags:"
+            echo "-h/--help: Show this help"
+            echo "-y/--noconfirm: Download specified system without confirmation. "
+            echo "-l/--list: List all of the available systems with it's corresponding number."
+            echo "-s/--system: Download systems specified in the comma-separated list. Example: 0,2,34"
+            exit 0;
+            ;;
+        -y | --noconfirm)
+            echo "-y/--noconfirm option specified. Script will download specified system without confirmation."
+            noconfirm=1
+            shift
+            ;;
+        -l | --list)
+            echo "List of supported systems:"
+            drawmenu
+            exit 0;
+            ;;
+        -s | --system)
+            echo "-s/--system option specified. Script will download systems with the following numbers: '$2'"
+            picked="$2"
+            quickmode
+            ;;
+        --) shift; 
+            break 
+            ;;
+    esac
 done
-
-if [ "$silent" != "1" ]; then helpsection; fi
 
 normalmode
